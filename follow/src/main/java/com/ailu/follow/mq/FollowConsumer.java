ae5992dec7cf5e3d2f69ed648de0a15af8ac5de1
艾禄 <pingdiguo@douyu.tv>
2024-02-24 21:43:16 +0800

    发布动态, 动态详情页

--- follow/src/main/java/com/ailu/follow/mq/FollowConsumer.java
+++ follow/src/main/java/com/ailu/follow/mq/FollowConsumer.java
@@ -1,10 +1,14 @@
 package com.ailu.follow.mq;
 
+import com.ailu.follow.model.redis.FollowCache;
 import com.google.gson.Gson;
+import generator.domain.FollowCnt;
 import generator.domain.Followee;
 import generator.domain.Follower;
+import generator.service.FollowCntService;
 import generator.service.FolloweeService;
 import generator.service.FollowerService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.MessageModel;
@@ -16,7 +20,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
-
+@Slf4j
 @Component
 @RocketMQMessageListener(
         topic = "${rocketmq.custom.follow-topic}",                      // 1.topic：消息的发送者使用同一个topic
@@ -33,21 +37,28 @@ public class FollowConsumer implements RocketMQListener<MessageExt> {
     @Resource
     FolloweeService followeeService;
 
+    @Resource
+    FollowCntService followCntService;
+
+    @Resource
+    FollowCache followCache;
+
     @Override
     public void onMessage(MessageExt message) {
 
         // 1.获取消息内容
         byte[] body = message.getBody();
         String msg = new String(body);
-        System.out.println("接收到消息：" + msg);
 
         // 2.处理消息
         // 反序列化消息 gson
         Gson gson = new Gson();
         FollowMessage followMessage = gson.fromJson(msg, FollowMessage.class);
+
+        log.info("接收到消息：{}", followMessage);
+
         switch (followMessage.getOpType()) {
             case 1:
-                System.out.println("关注消息：" + followMessage);
 
                 Follower follower = new Follower();
                 follower.setFromUid(followMessage.getFromUid());
@@ -74,10 +85,26 @@ public class FollowConsumer implements RocketMQListener<MessageExt> {
 
                 break;
             case 2:
-                System.out.println("取消关注消息：" + followMessage);
+
+                // 获取原来的关系
+                Follower follower1 = followerService.lambdaQuery().eq(Follower::getFromUid, followMessage.getFromUid()).eq(Follower::getToUid, followMessage.getToUid()).one();
+                // 原来是单向关注
+                if (follower1.getStatus() == 1) {
+                    followerService.lambdaUpdate().eq(Follower::getFromUid, followMessage.getFromUid()).eq(Follower::getToUid, followMessage.getToUid()).set(Follower::getStatus, 0).update();
+                    followeeService.lambdaUpdate().eq(Followee::getFromUid, followMessage.getFromUid()).eq(Followee::getToUid, followMessage.getToUid()).set(Followee::getStatus, 0).update();
+
+                    // from的关注数减一
+                    followCntService.lambdaUpdate().eq(FollowCnt::getUid, followMessage.getFromUid()).setSql("follow_cnt=follow_cnt-1").update();
+                    // to的粉丝数减一
+                    followCntService.lambdaUpdate().eq(FollowCnt::getUid, followMessage.getToUid()).setSql("fans_cnt=fans_cnt-1").update();
+                }
+
+
                 break;
             default:
-                System.out.println("未知消息：" + followMessage);
+                log.error("未知的操作类型：{}", followMessage.getOpType());
         }
+
+        // todo 删除缓存
     }
 }
